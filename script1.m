7bb625cc8c079715dfb27b99256825f654a57cbd
jeffrey <jeffrey.miog@gmail.com>
2016-03-25 13:35:56 +0100

    made in line commands, changed variable name concerning the delimiter, as we were actually looking at the RTcal

--- script1.m
+++ script1.m
@@ -1,18 +1,18 @@
 filename = 'signal.txt';
 e01 = importdata(filename);
-
+plot(e01);
 %sender data fields
-send_threshold = 0.35;
+send_threshold_y = 0.35;
 sample_threshold_send = 1;
 
 last_threshold_pass = 0;
-data_zero_lower_bound = 40;
-data_zero_upper_bound = 60;
-data_one_lower_bound = 70;
-data_one_upper_bound = 100;
-data_delimiter_lower_bound = 145;
-data_delimiter_upper_bound = 165;
-delim_found = 0;
+bit_zero_lower_bound = 40;
+bit_zero_upper_bound = 60;
+bit_one_lower_bound = 70;
+bit_one_upper_bound = 100;
+RTcal_size_lower_bound = 145;
+RTcal_size_upper_bound = 165;
+RTcal_found = 0;
 result_send = '';
 
 %tag response data fields
@@ -40,40 +40,50 @@ result_tag = '';
 
 
 for i= 1:2650
-	if e01(i) < send_threshold					%passing the threshold
-		if last_threshold_pass == 0 
-			last_threshold_pass = i;		%initializing the last_threshold pass for the first passing to avoid false first data symbol
-		end
-		if ((e01(i-1) > e01(i)) && (e01(i-1) > send_threshold))				%declining values, downward signal
+	%below threshold
+    if e01(i) < send_threshold_y					
+        %initializing the last_threshold pass for the first passing to avoid false first data symbol
+        if last_threshold_pass == 0 
+			last_threshold_pass = i;		
+        end
+        %declining values, downward signal
+        if ((e01(i-1) > e01(i)) && (e01(i-1) > send_threshold_y))				
 			current_threshold_pass = i;
-		elseif (e01(i+1) > e01(i) && (e01(i+1) > send_threshold))			%inclining values, upward signal
+            
+        %inclining values, upward signal
+		elseif (e01(i+1) > e01(i) && (e01(i+1) > send_threshold_y))			
 			current_threshold_pass = i+1;
 		end
 		
 		threshold_pass_diff = current_threshold_pass - last_threshold_pass;
 		
-		if ((threshold_pass_diff < data_zero_upper_bound) && (threshold_pass_diff > data_zero_lower_bound) && (delim_found == 1))
+		if ((threshold_pass_diff < bit_zero_upper_bound) && (threshold_pass_diff > bit_zero_lower_bound) && (RTcal_found == 1))
 			result_send = strcat(result_send, '0');
-		elseif ((threshold_pass_diff < data_one_upper_bound) && (threshold_pass_diff > data_one_lower_bound) && (delim_found == 1))
+		elseif ((threshold_pass_diff < bit_one_upper_bound) && (threshold_pass_diff > bit_one_lower_bound) && (RTcal_found == 1))
 			result_send = strcat(result_send, '1');
-		elseif ((threshold_pass_diff < data_delimiter_upper_bound) && (threshold_pass_diff > data_delimiter_lower_bound))
+		elseif ((threshold_pass_diff < RTcal_size_upper_bound) && (threshold_pass_diff > RTcal_size_lower_bound))
 			result_send = strcat(result_send,'d');
-			delim_found = 1;
+			RTcal_found = 1;
 		end
 		last_threshold_pass = current_threshold_pass;
 	end
 end
 
-fprintf('Sent message: %s \n', result_send);
+fprintf('Reader message: %s \n', result_send);
 
 for i= 2650:5000
-	if e01(i) < tag_threshold					%passing the threshold
-		if last_threshold_pass == 0 
-			last_threshold_pass = i;		%initializing the last_threshold pass for the first passing to avoid false first data symbol
-		end
-		if ((e01(i-1) > e01(i)) && (e01(i-1) > tag_threshold))				%declining values, downward signal
+    %passing the threshold
+	if e01(i) < tag_threshold					
+		if last_threshold_pass == 0
+            %initializing the last_threshold pass for the first passing to avoid false first data symbol
+			last_threshold_pass = i;		
+        end
+        
+        %declining values, downward signal
+		if ((e01(i-1) > e01(i)) && (e01(i-1) > tag_threshold))				
 			current_threshold_pass = i;
-		elseif (e01(i+1) > e01(i) && (e01(i+1) > tag_threshold))			%inclining values, upward signal
+        %inclining values, upward signal
+		elseif (e01(i+1) > e01(i) && (e01(i+1) > tag_threshold))			
 			current_threshold_pass = i+1;
 		end
 		
@@ -88,9 +98,11 @@ for i= 2650:5000
 		elseif ((threshold_pass_diff < tag_data_one_upper_bound) && (threshold_pass_diff > tag_data_one_lower_bound))
 			result_tag = strcat(result_tag, '1');
 		elseif ((threshold_pass_diff < tag_data_v_upper_bound) && (threshold_pass_diff > tag_data_v_lower_bound)) 
-			result_tag = strcat(result_tag, '0v');			%The value 0 before v misses a flank, but the zero is there so we add it here. (unique size of v)
+			%The value 0 before v misses a flank, but the zero is there so we add it here. (unique size of v)
+            result_tag = strcat(result_tag, '0v');			
 		elseif (((threshold_pass_diff/2) < tag_data_one_upper_bound) && ((threshold_pass_diff/2) > tag_data_one_lower_bound))
-			result_tag = strcat(result_tag, '1v');			%might a value 1v appear, we notice it if the flanks are twice the size of a normal 1
+			%might a value 1v appear, we notice it if the flanks are twice the size of a normal 1
+            result_tag = strcat(result_tag, '1v');			
 		end 
 		last_threshold_pass = current_threshold_pass;
 	end
